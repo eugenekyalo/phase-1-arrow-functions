8931f78edfcdf8b144c37c15734833625b0a8116
Eugene <yovlinpentdragonvii@gmail.com>
2024-03-27 22:38:51 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// Define divide function expression
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  const square = (num) => num * num;
+  
+  // add arrow function
+  const add = (num1, num2) => num1 + num2;
+  
+  // output the functions
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
+  
